9436d7ed28681de4ba0df819750520a3a7eb5796
nirpendra83 <93668666+nirpendra83@users.noreply.github.com>
2026-06-13 10:42:25 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,13 +5,3 @@ resource "aws_vpc" "vpc01" {
     Name = "Terraform"
   }
 }
-
-resource "aws_instance" "ec201" {
-  ami = "ami-0bdd88bd06d16ba03"
-  instance_type = var.type
-  key_name = "eks"
-  tags = {
-    Name = "ec2-test"
-  }
-
-}
